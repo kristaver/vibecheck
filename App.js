dca796480ef1d4b77afcb1aaa41f3384d6936cd2
kristaver <kristaver@gmail.com>
2022-12-07 12:57:56 +0100

    start of implemented context

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {useFonts} from 'expo-font'
+import { useFonts } from 'expo-font'
+import { createContext, useContext } from 'react';
 import HomeScreen from './screens/HomeScreen';
 import ChartScreen from './screens/ChartScreen'
 
 
+export const StateContext = createContext([])
 
 export default function App() {
 
@@ -18,21 +20,23 @@ export default function App() {
   }
   
   const Stack = createNativeStackNavigator();
-  
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name='Home'
-          component={HomeScreen}
-          options={{ title: 'welcome to the app bestie' }}
-        />
-        <Stack.Screen
-          name = 'Statistics'
-          component={ChartScreen}
-          options={{ title: 'statistics for u :>'}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <StateContext.Provider value={[0, 1, 2]}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name='Home'
+            component={HomeScreen}
+            options={{ title: 'welcome to the app bestie' }}
+          />
+          <Stack.Screen
+            name = 'Statistics'
+            component={ChartScreen}
+            options={{ title: 'statistics for u :>'}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </StateContext.Provider>
   );
 }
--- screens/ChartScreen.js
+++ screens/ChartScreen.js
@@ -1,22 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { StateContext } from '../App'
 import { View, Button} from 'react-native'
 import styles from '../styles/App.component.style'
 import Chart from '../components/Chart/Chart'
 import { scheduleNotificationAsync } from 'expo-notifications'
 import NotifTest from '../components/NotifTest'
 
-function ChartScreen({ navigation }) {
+function ChartScreen({ navigation, children }) {
   const sendNotif = () => {
     console.log('jad')
     scheduleNotificationAsync()
   }
+  const state = useContext(StateContext)
+  console.log(state)
   
   return (
+    <StateContext.Provider value={state}>
       <View style={styles.container}>
         <Chart/>
         {/* <Button title="notsdfif" onPress={sendNotif}/> */}
         <NotifTest />
+        
+        <Button>
+          {console.log(state, chilren)}
+          busttt
+        </Button>
       </View>
+    </StateContext.Provider>
   )
 }
 
